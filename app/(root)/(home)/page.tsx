ce3d5ff53f3ee5f344ee30f1a5b1c77e774a76a2
harshparmar25 <harshparmar2506@gmail.com>
2024-11-06 16:11:18 +0530

    make upcoming meeting text dynamic

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,43 @@
 "use client";
 
 import MeetingTypeList from "@/components/MeetingTypeList";
+import { useGetCalls } from "@/hooks/useGetCalls";
 import React from "react";
 
 const Home = () => {
+  const { upcomingCalls, isLoading } = useGetCalls();
+
+  const getUpcomingMeetingText = () => {
+    if (isLoading) {
+      return "Loading...";
+    }
+
+    if (upcomingCalls?.length && upcomingCalls[0].state?.startsAt) {
+      const today = new Date();
+      const meetingDate = new Date(upcomingCalls[0].state?.startsAt);
+      if (meetingDate.toDateString() === today.toDateString()) {
+        const time = meetingDate.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit" });
+        return "Upcoming Meeting Today at " + time;
+      }
+
+      const tomorrow = new Date();
+      tomorrow.setDate(today.getDate() + 1);
+      if (meetingDate.toDateString() === tomorrow.toDateString()) {
+        const time = meetingDate.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit" });
+        return "Upcoming Meeting Tomorrow at " + time;
+      }
+
+      const dayOfWeek = meetingDate.toLocaleDateString("en-US", {
+        weekday: "long",
+        month: "long",
+        day: "numeric",
+      });
+      const meetingTime = meetingDate.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit" });
+      return "Upcoming Meeting on " + dayOfWeek + " at " + meetingTime;
+    }
+    return "No Upcoming Meetings";
+  };
+
   const time = new Date().toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit" });
   const date = new Date().toLocaleDateString("en-US", {
     weekday: "long",
@@ -17,10 +51,10 @@ const Home = () => {
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <h2
-            className="glassmorphism max-w-[270px] rounded py-2
-          text-center text-base font-normal"
+            className="glassmorphism max-w-[350px] rounded py-2
+          text-center text-base font-normal "
           >
-            Upcoming Meeting at: 12:30 PM
+            {getUpcomingMeetingText()}
           </h2>
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
