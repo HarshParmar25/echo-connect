839533db2886a484ac6068c8b55bfa395d664ce7
harshparmar25 <harshparmar2506@gmail.com>
2024-11-20 12:08:49 +0530

    chore: redirect to home page after successful sign up add fallbackredirecturl

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -4,7 +4,10 @@ import React from "react";
 const SignUpPage = () => {
   return (
     <main className="flex h-screen w-full items-center justify-center">
-      <SignUp forceRedirectUrl={process.env.NEXT_PUBLIC_BASE_URL} />
+      <SignUp
+        forceRedirectUrl={process.env.NEXT_PUBLIC_BASE_URL}
+        fallbackRedirectUrl={process.env.NEXT_PUBLIC_BASE_URL}
+      />
     </main>
   );
 };
